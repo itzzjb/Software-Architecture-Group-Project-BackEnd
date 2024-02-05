67f5ab090feb38c3dd6a4cfdf22a951bd5a044c2
Januda Bethmin <desilvabethmin@gmail.com>
2024-02-05 17:08:15 +0530

    clean code
    Co-Authored-By: Thevindu Kevin <114279734+ThevinduKevin@users.noreply.github.com>
    Co-Authored-By: yasith-mayura <139609956+yasith-mayura@users.noreply.github.com>

--- src/main/java/com/softwarearchitecture/groupproject/GroupprojectApplication.java
+++ src/main/java/com/softwarearchitecture/groupproject/GroupprojectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GroupprojectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GroupprojectApplication.class, args);
 	}
-	
 }
