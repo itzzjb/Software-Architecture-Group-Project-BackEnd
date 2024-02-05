9d3b16f1a8acd2d89b5542a5eb3f6a0348e04f44
ThevinduKevin <thevindukevin@gmail.com>
2024-02-05 14:35:22 +0530

    thevindu test 01

--- src/main/java/com/softwarearchitecture/groupproject/GroupprojectApplication.java
+++ src/main/java/com/softwarearchitecture/groupproject/GroupprojectApplication.java
@@ -9,6 +9,7 @@ public class GroupprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GroupprojectApplication.class, args);
 		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
