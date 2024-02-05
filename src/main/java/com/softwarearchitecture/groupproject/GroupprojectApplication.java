85eb7fe7d003dbc3b1f11be141a95b36db3fef05
Januda Bethmin <desilvabethmin@gmail.com>
2024-02-05 17:06:49 +0530

    clean code

--- src/main/java/com/softwarearchitecture/groupproject/GroupprojectApplication.java
+++ src/main/java/com/softwarearchitecture/groupproject/GroupprojectApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GroupprojectApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(GroupprojectApplication.class, args);
 	}
+	
 }
