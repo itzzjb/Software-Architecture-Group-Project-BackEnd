d33b962254ac2d6db064ea1ea419cdc3ec2d36b2
Januda Bethmin <desilvabethmin@gmail.com>
2024-02-05 13:35:55 +0530

    intial commit

---
+++ src/test/java/com/softwarearchitecture/groupproject/GroupprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarearchitecture.groupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
