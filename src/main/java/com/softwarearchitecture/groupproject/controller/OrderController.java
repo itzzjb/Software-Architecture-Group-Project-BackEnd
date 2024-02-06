9abc7c18a1ac63492a3bddf65c953c90063351e0
Januda Bethmin <desilvabethmin@gmail.com>
2024-02-06 18:46:01 +0530

    Added all the order classes

---
+++ src/main/java/com/softwarearchitecture/groupproject/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.softwarearchitecture.groupproject.controller;
+import com.softwarearchitecture.groupproject.entity.Order;
+import com.softwarearchitecture.groupproject.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class OrderController {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @GetMapping("/order")
+    public Order getOrder(@RequestParam int id) {
+        return orderRepository.findById(id);
+    }
+
+    @PostMapping("/order")
+    public void addOrder(@RequestBody Order order) {orderRepository.save(order);}
+
+}
---
+++ src/main/java/com/softwarearchitecture/groupproject/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.softwarearchitecture.groupproject.repository;
+
+import com.softwarearchitecture.groupproject.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    Order findById(int Id);
+
+}
