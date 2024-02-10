4ba817e2f9599e326e2ce4a7618c9ad7b6e87515
Yasith-Mayura <mayurayasith@gmail.com>
2024-02-10 13:46:55 +0530

    Product entity class and repository interface created

---
+++ src/main/java/com/softwarearchitecture/groupproject/entity/Product.java
@@ -0,0 +1,31 @@
+package com.softwarearchitecture.groupproject.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table(name = "Product")
+public class Product {
+    @Id
+    @GeneratedValue
+    private int prodcutId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int categoryId;
+}
---
+++ src/main/java/com/softwarearchitecture/groupproject/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.softwarearchitecture.groupproject.repository;
+
+import com.softwarearchitecture.groupproject.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
