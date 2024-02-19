f419a496fe60ae747f9bc63fe95f53de8e428564
Yasith-Mayura <mayurayasith@gmail.com>
2024-02-19 10:30:31 +0530

    user controller class deleted

--- src/main/java/com/softwarearchitecture/groupproject/repository/UserRepository.java
+++ src/main/java/com/softwarearchitecture/groupproject/repository/UserRepository.java
@@ -3,9 +3,4 @@ import com.softwarearchitecture.groupproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-<<<<<<< HEAD
-=======
-    User findById(int Id);
->>>>>>> fc84f1f2ad3f40b878b8c94dadf63e7817bda1f1
-
 }
