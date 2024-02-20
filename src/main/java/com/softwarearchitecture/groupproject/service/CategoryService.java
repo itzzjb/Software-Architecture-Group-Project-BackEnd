2f93c3680415d04ee16f40d3debed5ecaf69c502
Visura Indula Malawara Arachchi <visuraindula2001@gmail.com>
2024-02-20 15:14:58 +0530

    updated category functionallity

---
+++ src/main/java/com/softwarearchitecture/groupproject/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.softwarearchitecture.groupproject.service;
+
+import com.softwarearchitecture.groupproject.dto.CategoryDto;
+
+public interface CategoryService {
+    void addCategory(CategoryDto categoryDto);
+}
