196a82cf3ecbe8bc1317032da1717d6f659e38b6
Januda Bethmin <desilvabethmin@gmail.com>
2024-02-20 19:06:43 +0530

    Get user functionality updated

--- src/main/java/com/softwarearchitecture/groupproject/service/UserRegisterService.java
+++ src/main/java/com/softwarearchitecture/groupproject/service/UserRegisterService.java
@@ -1,7 +1,10 @@
 package com.softwarearchitecture.groupproject.service;
 
 import com.softwarearchitecture.groupproject.dto.UserRegistrationDto;
+import com.softwarearchitecture.groupproject.entity.User;
 
 public interface UserRegisterService {
     void userRegistration(UserRegistrationDto userRegistrationDto);
+
+    User findUser(int id);
 }
