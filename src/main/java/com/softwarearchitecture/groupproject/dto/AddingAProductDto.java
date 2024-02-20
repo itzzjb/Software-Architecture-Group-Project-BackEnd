c458e47555fb8b91fe6e659376f3aeede907b0ca
Januda Bethmin <desilvabethmin@gmail.com>
2024-02-20 13:12:01 +0530

    Created AddingAProductDto

---
+++ src/main/java/com/softwarearchitecture/groupproject/dto/AddingAProductDto.java
@@ -0,0 +1,21 @@
+package com.softwarearchitecture.groupproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddingAProductDto {
+    private int productId;
+
+    private String productName;
+
+    private int quantity;
+
+    private int price;
+
+    private int categoryId;
+
+}
