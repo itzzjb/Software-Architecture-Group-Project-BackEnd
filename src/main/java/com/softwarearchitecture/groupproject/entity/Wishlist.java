151a3aa999f0fc61d21c2624a558af51468fb09f
ThevinduKevin <thevindukevin@gmail.com>
2024-02-21 23:27:04 +0530

    add foreign key to wishlist

--- src/main/java/com/softwarearchitecture/groupproject/entity/Wishlist.java
+++ src/main/java/com/softwarearchitecture/groupproject/entity/Wishlist.java
@@ -2,6 +2,8 @@ package com.softwarearchitecture.groupproject.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 // POJO class for Wishlist
 // Used lombok annotations for creating getters, setters and constructors
 @Data
@@ -12,11 +14,19 @@ import lombok.*;
 public class Wishlist {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "wish_id")
     public int wishlistId;
-    @Column
-    public int userId;
 
-    @Column
-    public int productId;
+    //onetoone - one wishlist has one user
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_user_id")
+    public User user;
 
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_wish_id", nullable = false,referencedColumnName = "wish_id")
+    public List<Product> product;
+
+    public Wishlist(int wishlistId, int userId, int productId) {
+    }
 }
