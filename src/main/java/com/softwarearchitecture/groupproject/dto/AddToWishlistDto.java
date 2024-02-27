5e767cffa46e78ac7d223530f37975f9dd954301
Januda Bethmin <desilvabethmin@gmail.com>
2024-02-27 09:25:03 +0530

    Added data annotation

--- src/main/java/com/softwarearchitecture/groupproject/dto/AddToWishlistDto.java
+++ src/main/java/com/softwarearchitecture/groupproject/dto/AddToWishlistDto.java
@@ -1,15 +1,12 @@
 package com.softwarearchitecture.groupproject.dto;
 
 import jakarta.persistence.Column;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Setter
-@Getter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class AddToWishlistDto {
     public int wishlistId;
     public int userId;
